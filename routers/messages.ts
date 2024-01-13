65e1eb29a2df6a558b87080c9fe56d8445d8bae0
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-14 02:10:23 +0600

    created messageRouter, added types, added path

---
+++ routers/messages.ts
@@ -0,0 +1,39 @@
+import {Router} from 'express';
+import {promises as fs} from 'fs';
+import {Message} from '../types';
+import crypto from 'crypto';
+
+const messagesRouter = Router();
+const path = '../messages';
+messagesRouter.get('/', async (req, res) => {
+  const files = await fs.readdir(path);
+  const recentFiles = files.slice(-5);
+  
+  const messages = await Promise.all(
+    recentFiles.map(async (file) => {
+      const filePath = `${path}/${file}`;
+      const content = await fs.readFile(filePath, 'utf-8');
+      return JSON.parse(content);
+    })
+  );
+  
+  res.json(messages);
+})
+
+messagesRouter.post('/', async (req, res) => {
+  const id = crypto.randomUUID();
+  const datetime = new Date().toISOString();
+  const fileName = `${datetime}.txt`;
+  const filePath = `${path}/${fileName}`;
+  
+  const message: Message = {
+    id,
+    message: req.body.message,
+    datetime,
+  };
+  
+  await fs.writeFile(filePath, JSON.stringify(message));
+  res.send(message);
+});
+
+export default messagesRouter;
