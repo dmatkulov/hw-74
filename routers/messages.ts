f33208ef6853a3fcc2aea91e0da89b64dc52f1e0
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-14 03:05:31 +0600

    added try catch

--- routers/messages.ts
+++ routers/messages.ts
@@ -6,34 +6,42 @@ import crypto from 'crypto';
 const messagesRouter = Router();
 const path = './messages';
 messagesRouter.get('/', async (req, res) => {
-  const files = await fs.readdir(path);
-  const recentFiles = files.slice(-5);
-  
-  const messages: Message[] = await Promise.all(
-    recentFiles.map(async (file) => {
-      const filePath = `${path}/${file}`;
-      const content = await fs.readFile(filePath, 'utf-8');
-      const message = JSON.parse(content);
-      return {...message};
-    })
-  );
-  res.send(messages);
-})
+  try {
+    const files = await fs.readdir(path);
+    const recentFiles = files.slice(-5);
+    
+    const messages: Message[] = await Promise.all(
+      recentFiles.map(async (file) => {
+        const filePath = `${path}/${file}`;
+        const content = await fs.readFile(filePath);
+        const message = JSON.parse(content.toString());
+        return {...message};
+      })
+    );
+    res.send(messages);
+  } catch (error) {
+    console.error('Error reading files:', error);
+  }
+});
 
 messagesRouter.post('/', async (req, res) => {
-  const id = crypto.randomUUID();
-  const datetime = new Date().toISOString();
-  const fileName = `${datetime}.txt`;
-  const filePath = `${path}/${fileName}`;
-  
-  const message: Message = {
-    id,
-    message: req.body.message,
-    datetime,
-  };
-  
-  await fs.writeFile(filePath, JSON.stringify(message));
-  res.send(message);
+  try {
+    const id = crypto.randomUUID();
+    const datetime = new Date().toISOString();
+    const fileName = `${datetime}.txt`;
+    const filePath = `${path}/${fileName}`;
+    
+    const message: Message = {
+      id,
+      message: req.body.message,
+      datetime,
+    };
+    
+    await fs.writeFile(filePath, JSON.stringify(message));
+    res.send(message);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 });
 
 export default messagesRouter;
